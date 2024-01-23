954b7aa897618578b556b3a4f1a23b015454d173
Michelle Lawand <mklawand@gmail.com>
2024-01-23 17:27:47 -0300

    exercicios 9 a 11

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
