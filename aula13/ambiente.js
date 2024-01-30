c557f4e8eb024902c1400937f5ef24460fe9876d
Michelle Lawand <mklawand@gmail.com>
2024-01-30 17:22:05 -0300

    do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+/*var contador = 1
+while ( contador <= 10) {
+    console.log(`Okay ${contador}`)
+    contador++ //c = c + 1
+}*/
+
+var contador = 1 
+do {
+    console.log(`Okay ${contador}`)
+    contador++
+} while ( contador <= 10)
---
+++ aula13/node_6405e53f4848b.tmp.js
@@ -0,0 +1,10 @@
+/*var contador = 1
+while ( contador <= 10) {
+    console.log(`Okay ${contador}`)
+    contador++ //c = c + 1
+}*/
+
+var contador = 1 
+do {
+    console.log(`Okay ${contador}`)
+} while ( contador <= 10)
