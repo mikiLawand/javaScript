813b5103d16b5df7e7e82750b09465d6ad6dcf40
Michelle Lawand <mklawand@gmail.com>
2024-02-01 17:33:00 -0300

    tabuada

--- aula14ex/ex18/script.js
+++ aula14ex/ex18/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+function tabuada() {
+    var num = document.getElementById('txtnum')
+    //pegando a area de tabuada
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um número válido!')
+    } else {
+        var n = Number(num.value)
+        //converte para numero
+
+        //criando a tabuada 
+        var c = 1
+        //limpando o select
+        tab.innerHTML = ''
+        while (c <= 10) {
+            //criando elementos dentro do select 
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
